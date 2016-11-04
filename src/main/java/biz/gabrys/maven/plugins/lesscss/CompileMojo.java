abe34a1d89e8ae529ff64663b913ef83f03c6a26
agabrys <adam.gabrys@live.com>
2016-11-04 20:56:01 +0100

    remove MojoExecutionException from execute and addDependenciesToClasspath methods

--- src/main/java/biz/gabrys/maven/plugins/lesscss/CompileMojo.java
+++ src/main/java/biz/gabrys/maven/plugins/lesscss/CompileMojo.java
@@ -22,7 +22,6 @@ import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
@@ -348,7 +347,7 @@ public class CompileMojo extends AbstractMojo {
         }
     }
 
-    public void execute() throws MojoExecutionException, MojoFailureException {
+    public void execute() throws MojoFailureException {
         logParameters();
         if (skip) {
             getLog().info("Skips job execution");
@@ -367,7 +366,7 @@ public class CompileMojo extends AbstractMojo {
         }
     }
 
-    private void addDependenciesToClasspath() throws MojoExecutionException {
+    private void addDependenciesToClasspath() {
         if (verbose) {
             getLog().info("Adding project dependencies to classpath...");
         }
