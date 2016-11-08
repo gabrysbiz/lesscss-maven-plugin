33202e0b8990ba7df63aa8109572e73392ae2593
agabrys <adam.gabrys@live.com>
2016-11-08 22:12:22 +0100

    Fix quality flaws

--- src/main/java/biz/gabrys/maven/plugins/lesscss/CompileMojo.java
+++ src/main/java/biz/gabrys/maven/plugins/lesscss/CompileMojo.java
@@ -264,7 +264,7 @@ public class CompileMojo extends AbstractMojo {
         final ParametersLogBuilder logger = new ParametersLogBuilder(getLog());
         logger.append("skip", skip);
         logger.append("verbose", verbose, new SimpleSanitizer(verbose, Boolean.TRUE));
-        logger.append("force", force, new SimpleSanitizer(!watch || watch && !force, Boolean.FALSE));
+        logger.append("force", force, new SimpleSanitizer(!watch || !force, Boolean.FALSE));
         logger.append("alwaysOverwrite", alwaysOverwrite, new SimpleSanitizer(!(!watch && force && !alwaysOverwrite), Boolean.TRUE));
         logger.append("sourceDirectory", sourceDirectory);
         logger.append("outputDirectory", outputDirectory);
